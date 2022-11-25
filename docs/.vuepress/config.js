6f85708d4a8b7aad7f3b4cd4902cc84c33c0c9d0
xinyuan <xinyuan@dtstack.com>
2022-11-25 14:10:47 +0800

    修复配置错误

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: 'TypeScript4 文档',
 	description: 'TypeScript4 最新官方文档翻译',
 	theme: 'reco',
-	base: '/boke/'
+	base: '/boke/',
 	themeConfig: {
 		subSidebar: 'auto',
 		nav: [{
